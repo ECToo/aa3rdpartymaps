7d2f97bd33fe909282e8853ac8c05940871c2f7f
davecarter85 <davecarter85@45841e7e-f339-11de-9a6a-9d1f184cc70b>
2010-04-12 04:13:07 +0000

    AA Time test version

---
+++ development/AATime/AATime/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace AATime
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int beats;
+        
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+           int seconds = Convert.ToInt16(TimeSpan.Parse(Convert.ToString(DateTime.UtcNow.AddHours(1).TimeOfDay)).TotalSeconds * 0.01157);
+
+           beats = Convert.ToInt32(seconds);
+           lblBeats.Text = Convert.ToString(seconds);
+           
+        }
+    }
+}
