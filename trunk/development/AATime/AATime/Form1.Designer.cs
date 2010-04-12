cdeeb5db4928cb1a34bbb164a15f228eaf9ec8be
davecarter85 <davecarter85@45841e7e-f339-11de-9a6a-9d1f184cc70b>
2010-04-12 04:13:07 +0000

    AA Time test version

---
+++ trunk/development/AATime/AATime/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace AATime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblBeats = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblBeats
+            // 
+            this.lblBeats.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblBeats.Font = new System.Drawing.Font("Arial Narrow", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBeats.Location = new System.Drawing.Point(3, 9);
+            this.lblBeats.Name = "lblBeats";
+            this.lblBeats.Size = new System.Drawing.Size(276, 52);
+            this.lblBeats.TabIndex = 0;
+            this.lblBeats.Text = "label1";
+            this.lblBeats.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 117);
+            this.Controls.Add(this.lblBeats);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBeats;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
